9985d38a6f641a38105c61815d3971f3ebd3fc19
Olalekan Lukman <lekan@Olalekans-MacBook-Pro.local>
2022-08-02 21:18:53 +0100

    update DB credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,11 +16,11 @@ import { WalletTransaction } from './transaction/transaction.entity';
     TypeOrmModule.forRoot(
     {
     type: 'postgres',
-    host:  process.env.POSTGRES_HOST || 'localhost',
+    host:  process.env.POSTGRES_HOST || 'kandula.db.elephantsql.com',
     port: parseInt(process.env.POSTGRES_PORT) || 5432,
-    username:  process.env.DB_USER || 'zuvy',
-    password: process.env.DB_PASSWORD || '1234',
-    database: process.env.DB_DATABASE || 'zuvy_wallet-service_db',
+    username:  process.env.DB_USER || 'fynfgutw',
+    password: process.env.DB_PASSWORD || 'L6FtWBgz7BAtVf45RE7aN9cZ6C-WbjHA',
+    database: process.env.DB_DATABASE || 'fynfgutw',
     entities: [User, Wallet, WalletTransaction],
     synchronize: true,
     migrationsRun: true
